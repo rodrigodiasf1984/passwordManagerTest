fdcf497e2ebd35abd5d3ee01a70f323d943d3d8f
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-11-07 01:26:05 +0100

    chore: added globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import { GlobalStyle } from './styles/globals';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes />
+        <GlobalStyle />
       </BrowserRouter>
     </div>
   );
