f4111a96041624ffbc027b98df13cb2e480c2fe9
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-11-09 22:53:41 +0100

    reafactor: separate styles

--- src/views/ProductInformation/index.tsx
+++ src/views/ProductInformation/index.tsx
@@ -1,18 +1,22 @@
-import { Typography, Box, Divider, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import { CustomBackground, Header } from 'components';
+import { useTranslation } from 'react-i18next';
 import React from 'react';
 import group3Image from 'assets/img/group-3.svg';
 import groupImage from 'assets/img/group.svg';
-
 import theme from 'theme';
-import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { Content } from './styles';
+import Footer from 'components/Footer';
+import {
+  Content,
+  CustomDivider,
+  ContentWrapper,
+  WrapperImagesAndDescription,
+  WrapperImageAndDescription,
+} from './styles';
 
 function Step1() {
   const { t } = useTranslation();
-  const history = useHistory();
+
   return (
     <CustomBackground>
       <Header currentStep={1} />
@@ -20,44 +24,16 @@ function Step1() {
         <Typography variant="h1" color={theme.palette.secondary.main}>
           {t('views.common.title')}
         </Typography>
-        <Divider
-          sx={{
-            marginTop: '1rem',
-            width: '2%',
-            borderBottomWidth: 3,
-          }}
-        />
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              marginTop: '1rem',
-              padding: '2rem 5rem',
-              flexDirection: 'row',
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'space-around',
-              }}
-            >
+        <CustomDivider />
+        <ContentWrapper>
+          <WrapperImagesAndDescription>
+            <WrapperImageAndDescription>
               <img src={groupImage} alt="group" />
               <Typography variant="h4" maxWidth={300} paddingTop={5}>
                 {t('views.common.imageLeftDescription')}
               </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'space-around',
-              }}
-            >
+            </WrapperImageAndDescription>
+            <WrapperImageAndDescription>
               <img src={group3Image} alt="group" />
               <Typography
                 variant="h4"
@@ -67,8 +43,8 @@ function Step1() {
               >
                 {t('views.common.imageRightDescription')}
               </Typography>
-            </Box>
-          </Box>
+            </WrapperImageAndDescription>
+          </WrapperImagesAndDescription>
           <Typography variant="h3" color={theme.palette.secondary.main}>
             {t('views.common.subtitle')}
           </Typography>
@@ -81,49 +57,8 @@ function Step1() {
           <Typography variant="h4" paddingTop={3}>
             {t('views.common.descritipion2')}
           </Typography>
-        </Box>
-        <Divider sx={{ paddingTop: '5rem', borderColor: '#ccd5da' }} />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Button
-            variant="text"
-            color="secondary"
-            sx={{
-              marginTop: '2rem',
-              display: 'flex',
-              alignSelf: 'flex-start',
-              width: '10rem',
-              fontWeight: 'bold',
-              height: '3rem',
-              textTransform: 'capitalize',
-            }}
-            onClick={() => history.goBack()}
-          >
-            {t('button.cancel')}
-          </Button>
-          <Button
-            onClick={() => history.push('/form')}
-            variant="contained"
-            color="secondary"
-            endIcon={<KeyboardArrowRightIcon />}
-            sx={{
-              marginTop: '2rem',
-              display: 'flex',
-              alignSelf: 'flex-end',
-              width: '10rem',
-              height: '3rem',
-              fontWeight: 'bold',
-              textTransform: 'capitalize',
-            }}
-          >
-            {t('button.next')}
-          </Button>
-        </Box>
+        </ContentWrapper>
+        <Footer />
       </Content>
     </CustomBackground>
   );
--- src/views/ProductInformation/styles.ts
+++ src/views/ProductInformation/styles.ts
@@ -1,10 +1,38 @@
 import styled from 'styled-components';
 import { converterToRem } from 'utils/converterToRem';
+import { Divider } from '@mui/material';
+import { styled as MuiStyled } from '@mui/material/styles';
 
-export const Content = styled.div`
+export const Content = styled.main`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
   padding: ${converterToRem(30)}rem ${converterToRem(100)}rem;
+`;
+
+export const CustomDivider = MuiStyled(Divider)`
+  border-bottom-width: ${converterToRem(5)}rem;
+  width:${converterToRem(40)}rem;
+  margin-top: ${converterToRem(10)}rem;
+`;
+
+export const ContentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const WrapperImagesAndDescription = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 1rem;
+  padding: 2rem 5rem;
+`;
+
+export const WrapperImageAndDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
