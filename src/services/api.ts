e353451c4ea12f9470eae25fe9b64fe765187dd0
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-11-09 18:42:10 +0100

    refactor: improve props

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,14 @@
-const PRUEBA_KO = 'pruebaKO123';
+const PRUEBA_KO = 'pruebaKO123@';
 
 const RESPONSE_OK = { status: 200 };
 const RESPONSE_KO = { status: 401 };
 
-const submitForm = (pass: string, _repass: any, _optionalQuestion: any) =>
-  new Promise((resolve, reject) =>
+type PromiseResponse = {
+  status: number;
+};
+
+const submitForm = (pass: string, _repass: string, _optionalQuestion: string) =>
+  new Promise<PromiseResponse>((resolve, reject) =>
     setTimeout(
       () => (pass !== PRUEBA_KO ? resolve(RESPONSE_OK) : reject(RESPONSE_KO)),
       3000,
